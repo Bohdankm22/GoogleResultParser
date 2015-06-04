f16444fddfc0beb65f1ed9ca67f926334105e481
Bohdan <bogdan.sharipov@yandex.ru>
2015-06-04 22:13:39 +0300

    Creation
    First point. Repository is created

---
+++ src/main/java/com/bohdan/edu/GoogleResultParser/App.java
@@ -0,0 +1,13 @@
+package com.bohdan.edu.GoogleResultParser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
