94636e065f857a898a5e82be42738da30c60a118
Bohdan <bogdan.sharipov@yandex.ru>
2015-06-06 01:50:16 +0300

    Almost ready making package
    New changes:
    -adding argument from command line
    -new static method for waiting page downloading
    -First trying to make executable .jar file

--- src/main/java/com/bohdan/edu/GoogleResultParser/GoogleResultParser.java
+++ src/main/java/com/bohdan/edu/GoogleResultParser/GoogleResultParser.java
@@ -1,5 +1,7 @@
 package com.bohdan.edu.GoogleResultParser;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,43 +11,48 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Example {
+public class GoogleResultParser {
 	public static void main(String[] args) throws Exception {
 		// The Firefox driver supports javascript
 		WebDriver driver = new FirefoxDriver();
-
+		// Getting users phrase for query
+		String request = args[0];
 		// Go to the Google Suggest home page
 		driver.get("http://www.google.com/webhp?complete=1&hl=en");
-
-		// Enter the query string "Cheese"
+		// waiting for page downloading until the element we want is visible or
+		// 5 seconds is over
 		WebElement query = driver.findElement(By.name("q"));
-		query.sendKeys("Cheese");
+		query.sendKeys(request);
 		WebElement button = driver.findElement(By.name("btnG"));
 		button.click();
-		// Sleep until the div we want is visible or 5 seconds is over
+		// waiting for page downloading until the element we want is visible or
+		// 5 seconds is over
 		wait(driver, By.className("r"));
 		// And now list the suggestions
 		List<WebElement> allResults = driver.findElements(By
 				.xpath("//h3[@class='r']/a"));
 		List<String> list = new ArrayList<String>();
-		for (WebElement result : allResults){
+		for (WebElement result : allResults) {
 			list.add(result.getAttribute("href"));
 		}
 		for (String link : list) {
 			driver.get(link);
+			// waiting for page downloading until the title is visible or 5
+			// seconds is over
 			wait(driver, By.tagName("title"));
 			String title = driver.getTitle();
 			System.out.println(title);
 		}
 
-		driver.quit();
+		driver.close();
 	}
 
 	private static void wait(WebDriver driver, By by) {
 		long end = System.currentTimeMillis() + 5000;
 		while (System.currentTimeMillis() < end) {
 			try {
-				WebElement resultDiv = driver.findElement(by);;
+				WebElement resultDiv = driver.findElement(by);
+				;
 				if (resultDiv.isDisplayed()) {
 					break;
 				}
